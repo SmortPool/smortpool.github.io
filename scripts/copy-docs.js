b4203e380c4997948e161da4dec03e889c37c1c7
msqaddura_ <msqaddura@gmail.com>
2020-12-06 23:16:23 +0200

    added docs

--- scripts/copy-docs.js
+++ scripts/copy-docs.js
@@ -1,8 +1,7 @@
 const sh = require('shelljs');
 const path = require('path');
 
-const destPath = path.resolve(path.dirname(__filename), '../dist');
+const destPath = path.resolve(path.dirname(__filename), '../dist/*');
 const docsPath = path.resolve(path.dirname(__filename), '../docs');
 
-sh.rm('-rf', `${destPath}/*`)
-sh.rm('-rf', `${docsPath}/*`)
+sh.cp('-R', destPath, docsPath);

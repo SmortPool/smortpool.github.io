fd88d55a5b4b4d68730674187a315a3e589ea971
martin <martin@heatsync.eu>
2021-06-29 15:02:53 +0300

    Updated calculator, added all the formulas

--- docs/js/add.js
+++
@@ -1,6 +0,0 @@
-$("#singleMonthConsumption, #priceofElectricity").keyup(function() {
-    if($('#singleMonthConsumption').val() != "" && $('#priceofElectricity').val() != "") {
-      var result = parseInt($('#singleMonthConsumption').val()) + parseInt($('#priceofElectricity').val());
-      $('#results').text(result);
-    }
-  });
